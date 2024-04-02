4ab6425f54608584be1c559cc795c9ef0a678964
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-04-02 16:42:40 +0200

    fix commit : correzione stampa in pagina

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-
 /**
  * Movie
+ * caratteristiche di un singolo movie
  */
 class Movie {
     public $title;
@@ -37,7 +37,7 @@ class Movie {
 $movie1 = new Movie(
     "C'è ancora domani",
     "Un viaggio nel passato neorealista tra povertà, maschi tossici e speranze per la figura femminile",
-    "Drama/Comedy",
+    "Drama",
     "Paola Cortellesi",
     "2023",
 );
@@ -47,7 +47,7 @@ $movie1 = new Movie(
 $movie2 = new Movie(
     "Split", 
     "Il film segue un uomo affetto da disturbo dissociativo dell'identità che rapisce e imprigiona tre ragazze adolescenti in una struttura sotterranea isolata.",
-    "Mistery/Horror",
+    "Mistery",
     "M. Night Shyamalan",
     "2016",
 
@@ -73,21 +73,29 @@ $movies = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <title>php-oop-1</title>
+     <!-- bootstrap -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- css -->
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <h1>Movies</h1>
-    <ul>
-        <?php
-        foreach($movies as $movie) {
-            echo "
-            <li>
-            TITOLO : " . $movie->title ." <br> DESCRIZIONE:" . $movie->description . " <br> GENERE: " . $movie->gender . " <br> DIRETTORE:" . $movie->director . " <br> ANNO:" . $movie->year . ";
-            </li>
-            ";
-            
-        }
-        ?>
-        </ul>
+    <div class="container">
+        
+        <h1 class="text-danger text-center mb-3">Movies</h1>
+        <ul>
+            <?php
+            foreach($movies as $movie) {
+                echo "
+                <li>
+                TITOLO : " . $movie->title ." <br> DESCRIZIONE: " . $movie->description . " <br> GENERE: " . $movie->gender . " <br> DIRETTORE: " . $movie->director . " <br> ANNO: " . $movie->year . ";
+                </li>
+                ";
+                
+            }
+            ?>
+            </ul>
+
+    </div>
         
         
 </body>
